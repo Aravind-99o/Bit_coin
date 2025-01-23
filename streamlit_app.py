f91cc4b624aca23a2d539dd13d3592b0c8ccf861
Aravind-99o <iaravind.99o@gmail.com>
2025-01-23 11:55:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,14 @@ with st.expander('Data'):
   st.write('**Raw data**')
   df = pd.read_csv('BTC-USD_stock_data.csv')
   df
+
+  st.write('**X**')
+  x = df.drop(['Close'],axis=1)
+  x
+
+  st.write('**y**')
+  y = df['Close']
+  y
+
+with st.expander('Data visualization'):
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')

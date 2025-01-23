19efe61d3b90ad340ccae6d662aec567b6356c85
Aravind-99o <iaravind.99o@gmail.com>
2025-01-23 20:10:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,7 +85,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 
 # # Split the data
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 # # Convert to numpy arrays
 # x_train = x_train.values

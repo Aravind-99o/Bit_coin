661cf4fc63ab5a3525fa9f7f7e594bc50189913d
Aravind-99o <iaravind.99o@gmail.com>
2025-01-23 19:57:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ x=df.drop(['Close'],axis=1)
 
 y=df['Close']
 
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
 
 # from sklearn.linear_model import LinearRegression

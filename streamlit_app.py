02e54028c855eb676b969ee34d020366a34b1fc7
Aravind-99o <iaravind.99o@gmail.com>
2025-01-23 19:57:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,7 +86,7 @@ y=df['Close']
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
 
-# from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 model=LinearRegression()
 model.fit(x_train,y_train)
 y_pred=model.predict(x_test)
